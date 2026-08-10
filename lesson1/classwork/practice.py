ccaa2a23dfbbf6ba3536590528770028591c7758
XDRowland <tianac2007@hotmail.com>
2026-08-10 20:46:27 +0000

    practice

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -1,24 +1,27 @@
 # Problem 1
 # Create a variable for your favorite number and print it.
-
-
+num = 4
+print(num)
 
 # Problem 2
 # Create a variable for your height in meters and print it.
-
-
+hight = 5658
+print(hight)
 
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
-
-
+x = "cat"
+print("My favorite animal is a", x)
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
-
-
-
+f_name = "Xavier"
+print("My name is", f_name)
+l_name = "Rowland"
+print("My last name is", l_name)
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+y = "Xavier", 11, "Rowland"
+print("my first name last name and age are", y)
