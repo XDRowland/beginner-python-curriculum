cf3e3981c5aa2947d84110d00aa0c1ecfeae6b51
XDRowland <tianac2007@hotmail.com>
2026-08-13 19:51:10 +0000

    lesson3

--- lesson3/classwork/logical_operators.py
+++ lesson3/classwork/logical_operators.py
@@ -2,7 +2,7 @@ age = int(input("How old are you? "))
 has_ticket = input("Do you have a movie ticket? (yes/no) ")
 
 if age >= 13 and has_ticket == "yes": # AND: both conditions must be true
-    print("You can enter the PG-13 movie.")
+    print("You can enter the legend of zelda movie.")
 else:
     print("Sorry, you can't watch")
 
@@ -19,13 +19,13 @@ homework_done = input("Did you do your homework? (yes/no) ")
 if not homework_done == "yes": #NOT: flips True to False and False to True
     print("Go finish your homework")
 else:
-    print("Nice job! You're all done")
+    print("Nice job! You can go play tears of the kingdom now")
 
 is_raining = input("Is it raining? (yes/no) ")
 has_umbrella = input("Do you have an umbrella? (yes/no) ")
 
 if is_raining == "yes" and not has_umbrella == "yes":
-    print("You might get! Stay inside.")
+    print("You might get wet! Stay inside.")
 elif is_raining == "yes" and has_umbrella == "yes":
     print("You're ready to go outside!")
 else:
